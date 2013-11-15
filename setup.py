0fb72716571be2716af0f90f91f78b0d36a41def
Samith Sandanayake <samiths@zone24x7.com>
2013-11-16 04:04:14 +0530

    setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+__author__ = 'samiths'
+
+from setuptools import setup
+
+setup(
+    name='python-finacc',
+    version='0.1.alpha',
+    description='A financial calculation library for python',
+    url='https://github.com/samithdisal/python-finacc',
+    author='Samith Sandanayake',
+    author_email='samithdisal@gmail.com',
+    license='MIT',
+    packages=['finacc'],
+    zip_safe=False
+)
